5d3cf1978da8905627c9dd9ec08544a187e53fdd
jessmoody <49352744+jessmoody@users.noreply.github.com>
2020-10-03 23:58:53 -0700

    Updating game to allow restart

--- virusgame.py
+++ virusgame.py
@@ -2,107 +2,9 @@
 import turtle
 import random
 import winsound
+import sys
 
-score = 0
-lives = 3
 
-wn = turtle.Screen()
-wn.title("MaskUp")
-wn.bgcolor("green")
-wn.bgpic("retro_city.gif")
-wn.setup(width=800, height=600)
-wn.tracer(0)
-
-wn.register_shape("human.gif")
-wn.register_shape("Evil-Virus.gif")
-wn.register_shape("surgical-mask.gif")
-
-# Add the player
-player = turtle.Turtle()
-player.speed(0)
-player.shape("human.gif")
-player.color("white")
-player.penup()
-player.goto(0, -250)
-player.direction = "stop"
-
-# Create a list of good guys
-good_guys = []
-
-# Add the good_guys
-for _ in range(3):
-    good_guy = turtle.Turtle()
-    good_guy.speed(0)
-    good_guy.shape("surgical-mask.gif")
-    good_guy.color("blue")
-    good_guy.penup()
-    good_guy.goto(-100, 250)
-    good_guy.speed = random.uniform(0.3, 2.0)
-    good_guys.append(good_guy)
-
-# Create a list of bad guys
-bad_guys = []
-
-# Add the bad_guys
-for _ in range(5):
-    bad_guy = turtle.Turtle()
-    bad_guy.speed(0)
-    bad_guy.shape("Evil-Virus.gif")
-    bad_guy.color("red")
-    bad_guy.penup()
-    bad_guy.goto(100, 250)
-    bad_guy.speed = random.uniform(0.3, 1.0)
-    bad_guys.append(bad_guy)
-
-# Make the pen
-pen = turtle.Turtle()
-pen.hideturtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.goto(0, 260)
-font = ("Courier", 24, "normal")
-pen.write("Score: {} Lives: {}".format(score, lives), align="center", font=font)
-
-''' def newGame_message():
-    message = turtle.Turtle()
-    message.hideturtle()
-    message.speed(0)
-    message.color("white")
-    message.penup()
-    message.goto(0, 0)
-    font = ("Courier", 24, "normal")
-    message.write("Press 'a' to start\n Press 'q' to quit", align="center", font=font) 
- '''
-
-# Make the message
-def show_message(score):
-    message = turtle.Turtle()
-    message.hideturtle()
-    message.speed(0)
-    message.color("white")
-    message.penup()
-    message.goto(0, 0)
-    font = ("Courier", 24, "normal")
-    message.write("GAME OVER: TOO MUCH EXPOSURE TO VIRUS\n Score: {}\n!MASK UP and STAY SAFE!".format(score), align="center", font=font) 
-
-# Functions
-def go_left():
-    player.direction = "left"
-
-def go_right():
-    player.direction = "right"
-
-def stop_player():
-    player.direction = "stop"
-
-# Keyboard Binding
-wn.listen()
-wn.onkeypress(go_left, "Left")
-wn.onkeyrelease(stop_player, "Left")
-wn.onkeypress(go_right, "Right")
-wn.onkeyrelease(stop_player, "Right")
 
 new_game = False
 
@@ -113,10 +15,113 @@ def toggle_new_game():
     else:
         new_game = False
 
+wn = turtle.Screen()
+wn.title("MaskUp")
+wn.bgcolor("green")
+wn.bgpic("retro_city.gif")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+wn.register_shape("human.gif")
+
 def game_loop():
-    global lives
-    global score
+    score = 0
+    lives = 3
     global new_game
+
+    wn.register_shape("Evil-Virus.gif")
+    wn.register_shape("surgical-mask.gif")
+
+    # Add the player
+    player = turtle.Turtle()
+    player.speed(0)
+    player.shape("human.gif")
+    player.color("white")
+    player.penup()
+    player.goto(0, -250)
+    player.direction = "stop"
+
+
+    # Create a list of good guys
+    good_guys = []
+
+    # Add the good_guys
+    for _ in range(3):
+        good_guy = turtle.Turtle()
+        good_guy.speed(0)
+        good_guy.shape("surgical-mask.gif")
+        good_guy.color("blue")
+        good_guy.penup()
+        good_guy.goto(-100, 250)
+        good_guy.speed = random.uniform(0.3, 2.0)
+        good_guys.append(good_guy)
+
+    # Create a list of bad guys
+    bad_guys = []
+
+    # Add the bad_guys
+    for _ in range(5):
+        bad_guy = turtle.Turtle()
+        bad_guy.speed(0)
+        bad_guy.shape("Evil-Virus.gif")
+        bad_guy.color("red")
+        bad_guy.penup()
+        bad_guy.goto(100, 250)
+        bad_guy.speed = random.uniform(0.3, 1.0)
+        bad_guys.append(bad_guy)
+
+   
+    # Make the pen
+    pen = turtle.Turtle()
+    pen.hideturtle()
+    pen.speed(0)
+    pen.shape("square")
+    pen.color("white")
+    pen.penup()
+    pen.goto(0, 260)
+    font = ("Courier", 24, "normal")
+    pen.write("Score: {} Lives: {}".format(score, lives), align="center", font=font)
+
+    ''' def newGame_message():
+        message = turtle.Turtle()
+        message.hideturtle()
+        message.speed(0)
+        message.color("white")
+        message.penup()
+        message.goto(0, 0)
+        font = ("Courier", 24, "normal")
+        message.write("Press 'a' to start\n Press 'q' to quit", align="center", font=font) 
+    '''
+
+    # Make the message
+    def show_message(score):
+        message = turtle.Turtle()
+        message.hideturtle()
+        message.speed(0)
+        message.color("white")
+        message.penup()
+        message.goto(0, 0)
+        font = ("Courier", 24, "bold")
+        message.write("GAME OVER: TOO MUCH EXPOSURE TO VIRUS\n Score: {}\n!MASK UP and STAY SAFE!".format(score), align="center", font=font) 
+
+    # Functions
+    def go_left():
+        player.direction = "left"
+
+    def go_right():
+        player.direction = "right"
+
+    def stop_player():
+        player.direction = "stop"
+
+    # Keyboard Binding
+    wn.listen()
+    wn.onkeypress(go_left, "Left")
+    wn.onkeyrelease(stop_player, "Left")
+    wn.onkeypress(go_right, "Right")
+    wn.onkeyrelease(stop_player, "Right")
+
+
+ 
     while True:
         # Update screen
         wn.update()
@@ -179,16 +184,17 @@ def game_loop():
                 pen.write("Score: {} Lives: {}".format(score, lives), align="center", font=font)
                 winsound.PlaySound("arcade_game_alarm_short", winsound.SND_FILENAME)
 
-        if lives < 0:
+        if lives <= 0:
             pen.clear()
             bad_guy.clear()
             good_guy.clear()
             show_message(score)
             winsound.PlaySound("game_over_sound", winsound.SND_FILENAME)
-            wn.listen()
-            wn.onkeypress(toggle_new_game, "a")
-            if new_game == True:
-                break
+            # wn.listen()
+            # if wn.onkeypress(toggle_new_game, "a"):
+            # if new_game == True:
+            break
+            # wn.onkeypress(sys.exit(), "q")
 
 while True:
     # Update screen
@@ -196,6 +202,9 @@ while True:
     # Play music
     winsound.PlaySound("retro_video_game_music", winsound.SND_LOOP)
     game_loop()
+    #turtle.resetscreen()
+
+    sys.exit()
     #newGame_message()
     
      
